aeeacd30859b98f709ff4137217ea2fcf12e473e
abderrahim <abderrahimadh@gmail.com>
2025-05-07 13:58:56 +0100

    first commit

---
+++ src/components/AboutMe/AboutMe.js
@@ -0,0 +1,44 @@
+import { AboutMeContainer, AboutMeBg, AboutMeLeftContainer, Img, AboutMeRightContainer, AboutMeInnerContainer, Title, Description, LineBreak, CVButton } from './AboutMeStyledComponent';
+import { personal_info } from '../../data/index';
+import BgAnimation from '../BgAnimation/BgAnimation';
+import TwitterIcon from '@mui/icons-material/Twitter';
+import EmailIcon from '@mui/icons-material/Email';
+import { IconButton, Box } from '@mui/material';
+
+const AboutMe = () => {
+    return (
+        <div id="AboutMe">
+            <AboutMeContainer>
+                <AboutMeBg>
+                    <BgAnimation />
+                </AboutMeBg>
+
+                <AboutMeInnerContainer>
+                    <AboutMeLeftContainer id="Left">
+                        <Title>ABOUT ME</Title>
+                        <Description>
+                        Experienced Full Stack Developer with exceptional problem-solving skills, proficient in building and deploying complete web applications using Next.js and React js. Skilled in Java, JavaScript, and AWS cloud services, with expertise in Dockerization and a proven record of impactful open-source contributions to various organizations including  GSSOC, Hacktoberfest .                        </Description>
+                        <LineBreak />
+                        <CVButton href={personal_info.cv} target='_blank' rel='noopener noreferrer'>View CV</CVButton>
+
+                        {/* Social Links */}
+                        <Box sx={{ marginTop: 3, display: 'flex', gap: 2 }}>
+                            <IconButton href="mailto:aashishaik526@example.com" target="_blank" rel="noopener noreferrer">
+                                <EmailIcon fontSize="large" sx={{ color: '#1DA1F2' }} />
+                            </IconButton>
+                            <IconButton href="https://x.com/_Aashii_1" target="_blank" rel="noopener noreferrer">
+                                <TwitterIcon fontSize="large" sx={{ color: '#1DA1F2' }} />
+                            </IconButton>
+                        </Box>
+                    </AboutMeLeftContainer>
+
+                    <AboutMeRightContainer id="Right">
+                        <Img src="./img/ashi.png" alt="AboutMe" />
+                    </AboutMeRightContainer>
+                </AboutMeInnerContainer>
+            </AboutMeContainer>
+        </div>
+    );
+}
+
+export default AboutMe;
---
+++ src/components/ContactMe/ContactMe.js
@@ -0,0 +1,73 @@
+import { Container, Wrapper, Title, Description, ContactForm, ContactTitle, ContactInput, ContactInputMessage, ContactSendButton } from './ContactMeStyledComponent';
+import React, { useRef } from 'react';
+import emailjs from '@emailjs/browser';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+const ContactMe = () => {
+
+    // Email JS API
+    const refForm = useRef();
+
+    const sendEmail = (event) => {
+        event.preventDefault();
+
+        emailjs
+            .sendForm(
+                "service_pq12zbb",
+                "template_bv9xyf6",
+                refForm.current,
+                "O00nt2KEZP8n3jiXD"
+            )
+            .then(
+                (result) => {
+                    // Show success toast notification
+                    toast.success("Message successfully sent!");
+                    refForm.current.reset();
+                },
+                (error) => {
+                    console.error(error.text);
+                    toast.error("Failed to send the message, please try again");
+                }
+            );
+    }
+
+    return (
+        <Container id="ContactMe">
+            <Wrapper>
+                <Title>CONTACT ME</Title>
+                <Description>Flick me a quick message :)</Description>
+                <ContactForm ref={refForm} onSubmit={sendEmail}>
+                    <ContactTitle>Get In Touch 🚀</ContactTitle>
+                    <ContactInput
+                        name="user_name"
+                        placeholder="Your Name"
+                        required
+                    />
+                    <ContactInput
+                        name="user_email"
+                        placeholder="Your Email"
+                        required
+                    />
+                    <ContactInput
+                        name="contact_number"
+                        placeholder="Your Contact Number"
+                        required
+                    />
+                    <ContactInputMessage
+                        name="message"
+                        placeholder="Leave your message"
+                        rows="8"
+                        required
+                    />
+                    <ContactSendButton type="submit" value="Send" />
+                </ContactForm>
+                
+                {/* Toast Container for notifications */}
+                <ToastContainer position="top-right" autoClose={6000} hideProgressBar={false} closeOnClick draggable pauseOnHover />
+            </Wrapper>
+        </Container>
+    );
+}
+
+export default ContactMe;
