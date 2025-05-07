b163f7e7d7a3b0ea0b1f37c9185cc8d53e9aea6a
abderrahimadh <149543236+abderrahimadh@users.noreply.github.com>
2025-05-07 23:31:20 +0100

    Update AboutMe.js

--- src/components/AboutMe/AboutMe.js
+++ src/components/AboutMe/AboutMe.js
@@ -1,8 +1,9 @@
 import { AboutMeContainer, AboutMeBg, AboutMeLeftContainer, Img, AboutMeRightContainer, AboutMeInnerContainer, Title, Description, LineBreak, CVButton } from './AboutMeStyledComponent';
 import { personal_info } from '../../data/index';
 import BgAnimation from '../BgAnimation/BgAnimation';
-import TwitterIcon from '@mui/icons-material/Twitter';
-import EmailIcon from '@mui/icons-material/Email';
+import InstagramIcon from '@mui/icons-material/Instagram';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import FacebookIcon from '@mui/icons-material/Facebook';
 import { IconButton, Box } from '@mui/material';
 
 const AboutMe = () => {
@@ -17,17 +18,25 @@ const AboutMe = () => {
                     <AboutMeLeftContainer id="Left">
                         <Title>ABOUT ME</Title>
                         <Description>
-                        Experienced Full Stack Developer with exceptional problem-solving skills, proficient in building and deploying complete web applications using Next.js and React js. Skilled in Java, JavaScript, and AWS cloud services, with expertise in Dockerization and a proven record of impactful open-source contributions to various organizations including  GSSOC, Hacktoberfest .                        </Description>
+                            I'm Abderrahim ADHAOUR, a second-year student at Mohammedia School of Engineering,
+                            diving deep into the realms of Computer Science and Digitalization. Fueled by a
+                            passion for DevOps and Cloud Engineering, I'm constantly honoriing my skills through
+                            competitive programming challenges and hands-on experiences.
+                            My academic journey is driven by a keen interest in DevOps and cloud technologies. With a solid foundation from the Mohammedia School of Engineering, I'm committed to expanding my expertise to tackle complex challenges in these fields. Beyond the classroom, I thrive on practical applications, seeking PFE Internship opportunities to further refine my skills and make meaningful contributions to innovative teams.</Description>
                         <LineBreak />
-                        <CVButton href={personal_info.cv} target='_blank' rel='noopener noreferrer'>View CV</CVButton>
+                        <CVButton onClick={() => window.open("./img/resume.pdf", '_blank')}>View CV</CVButton>
 
                         {/* Social Links */}
                         <Box sx={{ marginTop: 3, display: 'flex', gap: 2 }}>
-                            <IconButton href="mailto:aashishaik526@example.com" target="_blank" rel="noopener noreferrer">
-                                <EmailIcon fontSize="large" sx={{ color: '#1DA1F2' }} />
+                            <IconButton href="https://www.linkedin.com/in/abderrahim-adhaour-079b39278/" target="_blank"
+                                        rel="noopener noreferrer">
+                                <LinkedInIcon fontSize="large" sx={{ color: '#1DA1F2' }} />
                             </IconButton>
-                            <IconButton href="https://x.com/_Aashii_1" target="_blank" rel="noopener noreferrer">
-                                <TwitterIcon fontSize="large" sx={{ color: '#1DA1F2' }} />
+                            <IconButton href="https://www.instagram.com/adhabderrahim/" target="_blank" rel="noopener noreferrer">
+                                <InstagramIcon fontSize="large" sx={{ color: '#1DA1F2' }} />
+                            </IconButton>
+                            <IconButton href="https://www.facebook.com/abderrahim.adhaour" target="_blank" rel="noopener noreferrer">
+                                <FacebookIcon fontSize="large" sx={{ color: '#1DA1F2' }} />
                             </IconButton>
                         </Box>
                     </AboutMeLeftContainer>
